bff301a09b454672798dd03a13124b298b4f91e3
ThiGouvea <thiagouvea90@gmail.com>
2022-06-08 19:53:53 -0300

    reestruturando

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ ipsValidos = []
 arquivoIpsValidos = open('servers.txt', 'a')
 rangeInicial = []
 rangeFinal = []
+inicioIP = 0
+meioIP = 0
+
+inicioIP = int(input('Qual o primeiro numero do IP: '))
+meioIP = int(input('Qual o segundo numero do IP: '))
 
 print('Digite o range da parte do meio')
 rangeInicial.append(int(input('inicio: ')))
@@ -16,7 +21,7 @@ rangeFinal.append(int(input('fim: ')))
 
 for x in range(rangeInicial[0], rangeInicial[1], 1):
     for y in range(rangeFinal[0], rangeFinal[1], 1):
-        resultado = subprocess.run(['ping','-c 1', '-W 0.2', f'192.168.{x}.{y}'], capture_output=True)
+        resultado = subprocess.run(['ping', '-c 1', '-W 0.2', f'{inicioIP}.{meioIP}.{x}.{y}'], capture_output=True)
         if resultado.returncode == 0:
             ipsValidos.append(resultado.args[3])
             ipsValidos.append('\n')
